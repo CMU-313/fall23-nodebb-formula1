19c4f5bc5bfc573c8b2339dd1b4aee65733f5ef6
Ellen Fang <ellenfang77@ellens-mbp-6.wifi.local.cmu.edu>
2023-09-19 21:07:50 -0400

    translate online.ts to js

--- src/user/online.js
+++ src/user/online.js
@@ -1,43 +1,72 @@
-'use strict';
-
-const db = require('../database');
-const topics = require('../topics');
-const plugins = require('../plugins');
-const meta = require('../meta');
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import db from '../database';
+// import topics from '../topics';
+// import plugins from '../plugins';
+// import meta from '../meta';
+const db = require("../database");
+const topics = require("../topics");
+const plugins = require("../plugins");
+const meta = require("../meta");
+const User = {
+    uid: '',
+};
 module.exports = function (User) {
-    User.updateLastOnlineTime = async function (uid) {
-        if (!(parseInt(uid, 10) > 0)) {
-            return;
-        }
-        const userData = await db.getObjectFields(`user:${uid}`, ['status', 'lastonline']);
-        const now = Date.now();
-        if (userData.status === 'offline' || now - parseInt(userData.lastonline, 10) < 300000) {
-            return;
-        }
-        await User.setUserField(uid, 'lastonline', now);
+    User.updateLastOnlineTime = function (uid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!(parseInt(uid, 10) > 0)) {
+                return;
+            }
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const userData = yield db.getObjectFields(`user:${uid}`, ['status', 'lastonline']);
+            const now = Date.now();
+            if (userData.status === 'offline' || now - parseInt(userData.lastonline, 10) < 300000) {
+                return;
+            }
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+            yield User.setUserField(uid, 'lastonline', now);
+        });
     };
-
-    User.updateOnlineUsers = async function (uid) {
-        if (!(parseInt(uid, 10) > 0)) {
-            return;
-        }
-        const now = Date.now();
-        const userOnlineTime = await db.sortedSetScore('users:online', uid);
-        if (now - parseInt(userOnlineTime, 10) < 300000) {
-            return;
-        }
-        await db.sortedSetAdd('users:online', now, uid);
-        topics.pushUnreadCount(uid);
-        plugins.hooks.fire('action:user.online', { uid: uid, timestamp: now });
+    User.updateOnlineUsers = function (uid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!(parseInt(uid, 10) > 0)) {
+                return;
+            }
+            const now = Date.now();
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const userOnlineTime = yield db.sortedSetScore('users:online', uid);
+            if (now - parseInt(userOnlineTime, 10) < 300000) {
+                return;
+            }
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield db.sortedSetAdd('users:online', now, uid);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+            topics.pushUnreadCount(uid);
+            yield plugins.hooks.fire('action:user.online', { uid: uid, timestamp: now });
+        });
     };
-
-    User.isOnline = async function (uid) {
-        const now = Date.now();
-        const isArray = Array.isArray(uid);
-        uid = isArray ? uid : [uid];
-        const lastonline = await db.sortedSetScores('users:online', uid);
-        const isOnline = uid.map((uid, index) => (now - lastonline[index]) < (meta.config.onlineCutoff * 60000));
-        return isArray ? isOnline : isOnline[0];
+    User.isOnline = function (uid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const now = Date.now();
+            const isArray = Array.isArray(uid);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            uid = isArray ? uid : [uid];
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const lastonline = yield db.sortedSetScores('users:online', uid);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, max-len
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
+            const isOnline = uid.map((_uid, index) => (now - lastonline[index]) < (meta.config.onlineCutoff * 60000));
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+            return isArray ? isOnline : isOnline[0];
+        });
     };
 };
---
+++ src/user/online.ts
@@ -0,0 +1,72 @@
+// import db from '../database';
+// import topics from '../topics';
+// import plugins from '../plugins';
+// import meta from '../meta';
+import db = require('../database');
+import topics = require('../topics');
+import plugins = require('../plugins');
+import meta = require('../meta');
+
+interface userDataTemplate {
+    status: string;
+    lastonline: string;
+}
+
+interface UserTemplate {
+    uid: string;
+    updateLastOnlineTime?: any;
+    setUserField?: any;
+    updateOnlineUsers?: any;
+    isOnline?: any;
+}
+
+const User: UserTemplate = {
+    uid: '',
+};
+
+module.exports = function (User: UserTemplate) {
+    User.updateLastOnlineTime = async function (uid: string) {
+        if (!(parseInt(uid, 10) > 0)) {
+            return;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const userData: userDataTemplate = await db.getObjectFields(`user:${uid}`, ['status', 'lastonline']) as userDataTemplate;
+        const now = Date.now();
+        if (userData.status === 'offline' || now - parseInt(userData.lastonline, 10) < 300000) {
+            return;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        await User.setUserField(uid, 'lastonline', now);
+    };
+
+    User.updateOnlineUsers = async function (uid: string) {
+        if (!(parseInt(uid, 10) > 0)) {
+            return;
+        }
+        const now = Date.now();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const userOnlineTime: string = await db.sortedSetScore('users:online', uid) as string;
+        if (now - parseInt(userOnlineTime, 10) < 300000) {
+            return;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await db.sortedSetAdd('users:online', now, uid);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        topics.pushUnreadCount(uid);
+        await plugins.hooks.fire('action:user.online', { uid: uid, timestamp: now });
+    };
+
+    User.isOnline = async function (uid: any) {
+        const now = Date.now();
+        const isArray = Array.isArray(uid);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        uid = isArray ? uid : [uid];
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const lastonline : [] = await db.sortedSetScores('users:online', uid) as [];
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, max-len
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
+        const isOnline : any = uid.map((_uid, index) => (now - lastonline[index]) < (meta.config.onlineCutoff * 60000));
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+        return isArray ? isOnline : isOnline[0];
+    };
+};
