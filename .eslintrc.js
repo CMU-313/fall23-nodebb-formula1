6e306d12738e39e993e3b99de524b337f11532d7
Vanessa Lin <vanessa_lin@jtron-tech.com>
2023-10-26 22:23:57 -0400

    fixed line in src/cli/index.js causing error

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    parser: 'hermes-eslint',
+    plugins: [
+      'ft-flow'
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:ft-flow/recommended',
+    ],
+  };
+  
